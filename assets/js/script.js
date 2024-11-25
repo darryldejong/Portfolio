6e1fe1dbc0ac3870f1452e86c2b03cc080597b0e
Darryl de Jong <darryldjong@gmail.com>
2024-11-25 23:42:36 +0100

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,113 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  const links = document.querySelectorAll('a');
+
+  links.forEach(link => {
+    link.addEventListener('mouseover', function() {
+      link.style.textDecoration = 'underline';
+    });
+
+    link.addEventListener('mouseout', function() {
+      link.style.textDecoration = 'none';
+    });
+  });
+
+  const socialMediaText = document.querySelector('footer .socialmedia p');
+
+  if (socialMediaText) {
+    socialMediaText.addEventListener('mouseover', function() {
+      socialMediaText.style.textDecoration = 'underline';
+    });
+
+    socialMediaText.addEventListener('mouseout', function() {
+      socialMediaText.style.textDecoration = 'none';
+    });
+  }
+
+  const scrollToSection = (id) => {
+    const section = document.querySelector(id);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  const linksArray = [
+    { selector: 'a[href="#projects"]', target: '#projects' },
+    { selector: 'a[href="#contact"]', target: '#contact' },
+    { selector: 'a[href="#aboutme"]', target: '#aboutme' }
+  ];
+
+  linksArray.forEach(link => {
+    const anchor = document.querySelector(link.selector);
+    if (anchor) {
+      anchor.addEventListener('click', () => scrollToSection(link.target));
+    }
+  });
+
+  // particles
+  particlesJS("particlesjs", {
+    "particles": {
+      "number": {
+        "value": 60,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ff0000"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false
+      },
+      "size": {
+        "value": 3,
+        "random": true
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 3,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      }
+    },
+    "retina_detect": true
+  });
+
+});
