be0a44935d02060d3144401c34f850ae550295a3
Darryl de Jong <darryldjong@gmail.com>
2025-03-24 13:36:26 +0100

    Update redirect.js

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,7 +1,12 @@
 var userLanguage = navigator.language || navigator.userLanguage;
 
-if (userLanguage && userLanguage.startsWith("nl") && !window.location.pathname.startsWith("/nl")) {
-    window.location.href = "/nl";
-} else if (userLanguage && !userLanguage.startsWith("nl") && !window.location.pathname.startsWith("/en")) {
-    window.location.href = "/en";
+if (!localStorage.getItem("languageRedirected")) {
+    if (userLanguage.startsWith("nl") && !window.location.pathname.startsWith("/nl")) {
+        window.location.href = "/nl";
+        localStorage.setItem("languageRedirected", "true");
+    } 
+    else if (!userLanguage.startsWith("nl") && !window.location.pathname.startsWith("/en")) {
+        window.location.href = "/en";
+        localStorage.setItem("languageRedirected", "true");
+    }
 }
