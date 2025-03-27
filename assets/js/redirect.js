06cc248807af5f1dcad45b5e815ffb32ce42c998
Darryl de Jong <darryldjong@gmail.com>
2025-03-27 15:41:21 +0100

    Removed old files

--- assets/js/redirect.js
+++
@@ -1,9 +0,0 @@
-var userLanguage = navigator.language || navigator.userLanguage;
-
-if (window.location.pathname === "/") {
-    if (userLanguage.startsWith("nl")) {
-        window.location.href = "/nl";
-    } else {
-        window.location.href = "/en";
-    }
-}
