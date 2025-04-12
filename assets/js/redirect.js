b86426f3965fb244e483ec0614ddf3dd8269c7df
Darryl de Jong <darryldjong@gmail.com>
2025-04-13 00:34:53 +0200

    Add files via upload

---
+++ assets/js/redirect.js
@@ -0,0 +1,1 @@
+var userLanguage=navigator.language||navigator.userLanguage;if(window.location.pathname==="/"){if(userLanguage.startsWith("nl")){window.location.href="/nl"}else{window.location.href="/en"}}
